4b902bb8234d33f1f156c628f5d0f90e98e98601
haeseop jang <jhaeseop@gmail.com>
2022-12-23 13:10:49 +0900

    first commit

---
+++ config/production.py
@@ -0,0 +1,37 @@
+from logging.config import dictConfig
+
+from config.default import *
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user=os.getenv('DB_USER'),
+    pw=os.getenv('DB_PASSWORD'),
+    url=os.getenv('DB_HOST'),
+    db=os.getenv('DB_NAME'))
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = b'Zb3\x81\xdb\xf1\xd9\xd7-Knb\x8eB\xa5\x18'
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
+    }
+})
